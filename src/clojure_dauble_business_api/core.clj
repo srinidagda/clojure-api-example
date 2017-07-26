1f06f6a9703a67ae1ce8f68e8571619a5672f7ce
Srini <srini@srini-mac-pro.local>
2017-07-26 17:43:38 +0530

    Fetching Header token for authentication

--- src/clojure_dauble_business_api/core.clj
+++ src/clojure_dauble_business_api/core.clj
@@ -3,13 +3,16 @@
             [ring.util.http-response :refer :all]
             [clojure-dauble-business-api.logic :as logic]
             [clojure.tools.logging :as log]
-            [clojure-dauble-business-api.domain.artwork])
+            [clojure-dauble-business-api.domain.artwork]
+            [cheshire.core :as json])
   (:import [clojure_dauble_business_api.domain.artwork Artwork]))
 
 
 (defapi app
-  (GET ["/hello/:id", :id #"[0-9]+"] [id]
-    (log/info "Function begins from here" id)
+  (GET ["/hello/:id", :id #"[0-9]+"] [id :as request]
+    (log/info "Function begins from here" request)
+    (def jsonString (json/generate-string (get-in request [:headers])))
+    (log/info "Create - header value is " (get-in (json/parse-string jsonString true) [:accesstoken]))
     (def artworkData (logic/artwork-id (->> id (re-find #"\d+") Long/parseLong)))
     (def data (if (not-empty artworkData)
                {:data artworkData :status 200}
@@ -17,4 +20,5 @@
    (ok data))
   (POST "/create" params
    (log/info "Create - Function begins from here and body" (get-in params [:params :artwork :name]))
+   (log/info "Create - header value is " (get-in params [:headers]))
    (ok {:artwork (logic/create-city (get-in params [:params :artwork :name]))})))
--- src/clojure_dauble_business_api/test.clj
+++ src/clojure_dauble_business_api/test.clj
@@ -1,8 +1,13 @@
 (ns clojure-dauble-business-api.test
   (:require [compojure.api.sweet :refer :all])
-  (:require [ring.util.http-response :refer :all]))
+  (:require [ring.util.http-response :refer :all]
+            [clojure.tools.logging :as log]
+            [cheshire.core :as json]))
 
 (defapi test
-  (GET "/ping" []
+  (GET "/ping" params
     :query-params [value :- String]
-    (ok {:message (str "Hello " value)})))
+    (log/info "Create - header value is " params)
+    (def jsonString (json/generate-string (get-in params [:headers])))
+    (log/info "Create - header value is " (get-in (json/parse-string jsonString true) [:accesstoken]))
+   (ok {:message (str "Hello " value)})))
